d061083703526562713ff88abba8fa21236187c0
Victor <hellovictorlee@gmail.com>
2020-11-12 00:39:47 -0800

    dynamodb attribute schema convert

--- dynamodb.py
+++ dynamodb.py
@@ -36,7 +36,7 @@ def create_table(table='cache', region='us-west-2'):
 def put(item, table='cache', region='us-west-2'):
     try:
         dynamodb = boto3.client('dynamodb', region_name=region)
-        return dynamodb.put_item(TableName=table, Item=item)
+        return dynamodb.put_item(TableName=table, Item=convert(item))
     except ClientError as e:
         logging.error(e)
         return None
@@ -45,36 +45,66 @@ def put(item, table='cache', region='us-west-2'):
 def get(key, table='cache', region='us-west-2'):
     try:
         dynamodb = boto3.client('dynamodb', region_name=region)
-        return dynamodb.get_item(TableName=table, Key=key)
+        data = dynamodb.get_item(TableName=table, Key=convert(key))
+        return revert(data['Item'])
     except ClientError as e:
         logging.error(e)
         return None
 
 
+def convert(item):
+    if type(item) == dict:
+        mapper = dict()
+        for key, val in item.items():
+            _type = type(val)
+            if _type in (float, int):
+                mapper[key] = {'N': val}
+            elif _type == str:
+                mapper[key] = {'S': val}
+            elif _type == list:
+                mapper[key] = {'L': convert(val)}
+        return mapper
+    elif type(item) == list:
+        lst = list()
+        for val in item:
+            _type = type(val)
+            if _type in (float, int):
+                lst.append({'N': val})
+            elif _type == str:
+                lst.append({'S': val})
+            elif _type == list:
+                lst.append({'L': convert(val)})
+        return lst
+
+
+def revert(item):
+    if type(item) == dict:
+        mapper = dict()
+        for key, val in item.items():
+            if key in ('S', 'N'):
+                return val
+            elif key == 'L':
+                return revert(val)
+            else:
+                mapper[key] = revert(val)
+        return mapper
+    elif type(item) == list:
+        lst = list()
+        for val in item:
+            lst.append(revert(val))
+        return lst
+
+
 if __name__ == '__main__':
     # create_table()
 
-    epoch = math.floor(time.time())
-    item = {
-        'pk': {
-            'S': 'test'
-        },
-        'sk': {
-            'S': str(epoch)
-        },
-        'keys': {
-            'SS': ['hello', 'world']
-        }
-    }
-    put(item)
+    # epoch = math.floor(time.time())
+    # item = {'pk': 'test', 'sk': str(epoch), 'keys': ['hello', 'world']}
+    # put(item)
 
     # key = {
-    #     'pk': {
-    #         'S': 'test'
-    #     },
-    #     'sk': {
-    #         'S': str(epoch)
-    #     },
+    #     'pk': 'test',
+    #     'sk': '1605168923'
     # }
     # res = get(key)
     # print(res)
